eea180318d79eef68d129d6daf43c7af6c7cca7b
Enes Karacomak <eneskaracomak@hotmail.com>
2025-01-11 15:44:02 +0300

    version check add

--- lib/app/tabs/home/splash_screen.dart
+++ lib/app/tabs/home/splash_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:food_bit_app/app/app_garson.dart';
 import 'package:food_bit_app/app/components/FirebaseService.dart';
@@ -5,7 +8,9 @@ import 'package:food_bit_app/app/tabs/home/loginPage.dart';
 import 'dart:async';
 
 import 'package:food_bit_app/app/app.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,6 +39,8 @@ class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _logoAnimation;
+ final DatabaseReference _databaseReference =
+      FirebaseDatabase.instance.ref('Settings/Version');
 
   @override
   void initState() {
@@ -49,9 +56,112 @@ class _SplashScreenState extends State<SplashScreen>
     _controller.forward();
 
     // Oturum kontrolünü yap
-    _checkSession();
+    _checkForUpdate();
   }
 
+  Future<void> _checkForUpdate() async {
+    try {
+      // Uygulamanın mevcut sürümünü al
+      PackageInfo packageInfo = await PackageInfo.fromPlatform();
+       String version = packageInfo.version; // Örn: "1.0.0"
+    String buildNumber = packageInfo.buildNumber; // Örn: "9"
+
+    // "1.0.0" olan version bilgisini "1.0.9" formatına dönüştür
+    List<String> versionParts = version.split('.');
+    if (versionParts.length == 3) {
+      versionParts[2] = buildNumber; // Son kısmı buildNumber ile değiştir
+    }
+    String currentVersion = versionParts.join('.'); // "1.0.9"
+
+      // Firebase'den en son sürümü al
+      final DataSnapshot snapshot = await _databaseReference.get();
+
+      if (snapshot.exists) {
+        String latestVersion = snapshot.value.toString();
+        print(currentVersion);
+        print(latestVersion);
+        if (_isVersionOutdated(currentVersion, latestVersion)) {
+          _showUpdateDialog();
+        } else {
+          _checkSession();
+        }
+      } else {
+        // Firebase'den veri alınamazsa direkt giriş yapılır
+        _checkSession();
+      }
+    } catch (e) {
+      _checkSession();
+    }
+  }  
+  bool _isVersionOutdated(String currentVersion, String latestVersion) {
+    List<int> current = currentVersion.split('.').map(int.parse).toList();
+    List<int> latest = latestVersion.split('.').map(int.parse).toList();
+
+    for (int i = 0; i < latest.length; i++) {
+      if (i >= current.length || current[i] < latest[i]) return true;
+      if (current[i] > latest[i]) return false;
+    }
+    return false;
+  }
+
+  
+  void _showUpdateDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Güncelleme Mevcut'),
+          content: const Text(
+              'Yeni bir sürüm mevcut. Güncelleyerek en iyi deneyimi yaşayın.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+    if (Platform.isAndroid) {
+      _launchPlayStore(); // Android için Play Store'a yönlendirme
+    } else if (Platform.isIOS) {
+      _launchAppStore(); // iOS için App Store'a yönlendirme
+    }
+  },
+              child: const Text('Güncelle'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+  
+  void _launchPlayStore() async {
+    const url =
+        'https://play.google.com/store/apps/details?id=com.alminacafe.alminacafe';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+  void _launchAppStore() async {
+    const url =
+        'https://apps.apple.com/tr/app/almina-cafe/id6740246836?l=tr';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+   void _navigateToNextScreen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var isUserLoggedIn = prefs.getString('user');
+
+    Timer(const Duration(seconds: 3), () {
+      if (isUserLoggedIn != null) {
+        Navigator.pushReplacementNamed(context, '/Main');
+      } else {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+      }
+    });
+  }
   Future<void> _checkSession() async {
     FirebaseService service = FirebaseService();
     final prefs = await SharedPreferences.getInstance();
